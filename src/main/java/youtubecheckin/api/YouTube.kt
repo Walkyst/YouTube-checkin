d59bdfb6ddbda19ff38563a61e5e8016a9027721
Walkyst <41806921+Walkyst@users.noreply.github.com>
2021-12-27 05:41:50 +0300

    init

---
+++ src/main/java/youtubecheckin/api/YouTube.kt
@@ -0,0 +1,57 @@
+package youtubecheckin.api
+
+import org.apache.http.client.HttpClient
+import org.apache.http.client.config.RequestConfig
+import org.apache.http.config.RegistryBuilder
+import org.apache.http.conn.socket.ConnectionSocketFactory
+import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager
+import org.json.JSONObject
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import youtubecheckin.core.DroidConnectionSocketFactory
+import youtubecheckin.core.com.akdeniz.googleplaycrawler.GooglePlayAPI
+
+@RestController
+class YouTube {
+    @PostMapping("/youtube", produces = [MediaType.APPLICATION_JSON_VALUE])
+    private fun processRequest(@RequestBody res: String): ResponseEntity<String> {
+        val response = JSONObject()
+
+        try {
+            val request = JSONObject(res)
+            val api = GooglePlayAPI(request.get("email").toString(), request.get("password").toString())
+
+            api.client = createLoginClient()
+            api.login()
+            api.checkin()
+            api.uploadDeviceConfig()
+
+            response.put("androidId", api.androidID)
+            response.put("email", api.email)
+            response.put("aas_et", api.aas_et)
+        } catch (e: Exception) {
+            response.put("exception", e.message)
+            return ResponseEntity(response.toString(), HttpStatus.BAD_REQUEST)
+        }
+
+        return ResponseEntity(response.toString(), HttpStatus.OK)
+    }
+
+    private fun createLoginClient(): HttpClient {
+        val connManager = PoolingHttpClientConnectionManager(
+            RegistryBuilder.create<ConnectionSocketFactory>().register("https", DroidConnectionSocketFactory()).build()
+        )
+        connManager.maxTotal = 100
+        connManager.defaultMaxPerRoute = 30
+        val config = RequestConfig.custom()
+            .setConnectTimeout(10000)
+            .setConnectionRequestTimeout(10000)
+            .setSocketTimeout(10000).build()
+        return HttpClientBuilder.create().setDefaultRequestConfig(config).setConnectionManager(connManager).build()
+    }
+}
---
+++ src/main/java/youtubecheckin/core/com/akdeniz/googleplaycrawler/GooglePlayException.java
@@ -0,0 +1,48 @@
+package youtubecheckin.core.com.akdeniz.googleplaycrawler;
+
+import youtubecheckin.core.com.akdeniz.googleplaycrawler.GooglePlay.ResponseWrapper;
+import org.apache.http.HttpResponse;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class GooglePlayException extends IOException {
+	private static final long serialVersionUID = 1L;
+
+	private final int httpStatus;
+
+	public GooglePlayException(int httpStatus, String message) {
+		super(message);
+		this.httpStatus = httpStatus;
+	}
+
+	public int getHttpStatus() {
+		return httpStatus;
+	}
+
+	public static GooglePlayException create(HttpResponse httpResponse) {
+		String message = httpResponse.getStatusLine().getReasonPhrase();
+
+		// If the response contains a Protobuf response, retrieves the message from a
+		// ResponseWrapper object
+		InputStream content = null;
+
+		try {
+			content = httpResponse.getEntity().getContent();
+			if ("application/protobuf".equals(httpResponse.getEntity().getContentType().getValue())) {
+				ResponseWrapper rw = ResponseWrapper.parseFrom(content);
+
+				if (rw.hasCommands() && rw.getCommands().hasDisplayErrorMessage()) {
+					message = rw.getCommands().getDisplayErrorMessage();
+				}
+			}
+		}
+		catch (Exception ignored) {}
+
+		try {
+			content.close();
+		} catch (IOException ignored) {}
+
+		return new GooglePlayException(httpResponse.getStatusLine().getStatusCode(), message);
+	}
+}
